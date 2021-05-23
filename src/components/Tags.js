5cd742c16774c6fb018a66b513b7d794c9158e5a
Alex Hernandez <alexandre_2420@outlook.com>
2021-05-23 15:21:07 -0500

    add verifyfilters function

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -7,14 +7,33 @@ import styles from '../styles/components/Tags.module.css'
 // ______________________________________________________________________
 
 const Tags = (props) => {
-    const { setFilters } = useContext(AppContext)
+    const { filters, setFilters } = useContext(AppContext)
+
+    const filtersFlat = () => {
+       let count = []
+
+        filters.forEach( item => {
+            count.push(Object.values(item))
+        })
+
+        return count
+    }
+
+    const verifyFilterExist = (count, value, newTag) => {
+        if(count.flat().includes(value)){
+            return false
+        } else {
+            setFilters(filters => [ ...filters, newTag ])
+        }
+    }
 
     const handleOnClick = (e) => {
         let key = e.target.parentElement.value || e.target.value
         let value = e.target.innerText 
         let newTag = { [key] : value }
 
-        setFilters(filters => [ ...filters, newTag ])
+        let count =  filtersFlat()
+        verifyFilterExist(count, value, newTag)
     }
 
     return(
